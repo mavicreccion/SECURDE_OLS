663d4addb7f5a35580d903129c04279601e3dc0d
fuouo <fuouokun@gmail.com>
2017-07-01 21:24:36 +0800

    finished pages: homepage, sign in, registration

---
+++ mco1/js/app.js
@@ -0,0 +1,36 @@
+var sampleUser = {firstName: "Dyan Raisa",
+          lastName: "Nieva",
+          idNumber:11400366,
+          }
+
+var user = sampleUser;
+
+
+$(document).ready(function(){
+
+	initUserLink();
+	initSearchToggle();
+});
+
+
+function initUserLink(){
+  if(user != null){
+    $('#account-name').text(user.firstName + " " + user.lastName);
+    $('#account-name').parent("a").attr('href', 'profile.html');
+    //$('#account-name').("href", "http://www.google.com/");
+  }
+}
+
+function initSearchToggle(){
+  /* do not put in index.html */
+  $('#toggle-search').click(function(){
+    $('#overlay-screen').fadeIn("fast", function(){})
+    $('#content-search').fadeIn(350, function(){});
+    $('#search-bar').focus();
+  })
+
+  $('#overlay-screen').click(function(){
+      $('#overlay-screen').fadeOut("fast", function(){})
+      $('#content-search').fadeOut(350, function(){});
+  })
+}
---
+++ mco1/js/register.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  if(user != null){
+    window.location = "index.html";
+  }
+
+	initBdayDatePicker();
+});
+
+
+
+function initBdayDatePicker(){
+  var currentDate = {
+    date: new Date().getDate(),
+    year: new Date().getFullYear(),
+    month: new Date().getMonth(),
+  }
+  var startDate = new Date(currentDate.year-60, currentDate.month, currentDate.date);
+  var endDate = new Date(currentDate.year+60, currentDate.month, currentDate.date);
+  $('#bdaypicker').datepicker({
+    format:"yyyy-mm-dd",
+    startDate: startDate,
+    endDate: endDate,});
+  $('#bdaypicker').datepicker('update', currentDate.year + '-' + currentDate.month + '-' + currentDate.date);
+}
+
+
